9de50599ecd3113b8b45b779c41e7923e8960f1a
awais1019 <120665004+awais1019@users.noreply.github.com>
2022-12-26 19:42:58 +0500

    Add files via upload

---
+++ awa.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{ 
+int m_b;
+cout<<"Enter mbs:";
+cin>>m_b;
+int bits;
+bits=m_b*1;
+bits=m_b*1024*1024*8;
+cout<<"MBS IN BITS ARE"<<bits;
+}
+
---
+++ dollor.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+float dollor=222.5;
+int input;
+float total;
+cout<<"enter dollor:";
+cin>>input;
+total=dollor*input;
+cout<<"in rupees:"<<total;
+}
